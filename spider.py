9303b249651621028b4edf1dd80712f17242ffb2
apokar <apokar@163.com>
2017-07-19 12:02:30 +0800

    输出页面的商品信息

--- spider.py
+++ spider.py
@@ -1,6 +1,6 @@
 import os
 import re
-
+from pyquery import PyQuery as pq
 from selenium import webdriver
 
 # Explicit Waits
@@ -29,6 +29,7 @@ def search():
         input.send_keys('美食')
         submit.click()
         total = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"#mainsrp-pager > div > div > div > div.total")))
+        get_products()
         return total.text
     except TimeoutException:
         return search()
@@ -45,9 +46,25 @@ def next_page(page_number):
         input.send_keys(page_number)
         submit.click()
         wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,"#mainsrp-pager > div > div > div > ul > li.item.active > span"),str(page_number)))
+        get_products()
     except TimeoutException:
         next_page(page_number)
 
+def get_products():
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"#mainsrp-itemlist .items .item")))
+    html = browser.page_source
+    doc = pq(html)
+    items = doc("#mainsrp-itemlist .items .item").items()
+    for item in items:
+        product = {
+            'img' : item.find('.pic .img').attr('src'),
+            'price': item.find('.price').text(),
+            'deal': item.find('deal-cnt').text()[:-3],
+            'title':item.find('.title').text(),
+            'shop':item.find('.shop').text(),
+            'location':item.find('.location').text()
+        }
+        print(product)
 
 def main():
     total = search()
