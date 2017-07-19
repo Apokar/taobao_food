f6723f3211e9028a5472672973c729fe3a5f9c9e
apokar <apokar@163.com>
2017-07-19 12:20:17 +0800

    存储商品信息到mongo

---
+++ config.py
@@ -0,0 +1,4 @@
+db_host = 'localhost'
+db_port = 27017
+db_name = 'TB_meishi'
+db_collection = 'product_table'
--- spider.py
+++ spider.py
@@ -2,6 +2,12 @@ import os
 import re
 from pyquery import PyQuery as pq
 from selenium import webdriver
+from config import *
+import pymongo
+
+client = pymongo.MongoClient(db_host)
+db = client[db_name]
+db.product_table.drop()
 
 # Explicit Waits
 from selenium.common.exceptions import TimeoutException
@@ -9,13 +15,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-
-chromedriver ='/usr/local/bin/chromedriver'
+chromedriver = '/usr/local/bin/chromedriver'
 os.environ["webdriver.chrome.driver"] = chromedriver
 
 browser = webdriver.Chrome(chromedriver)
 wait = WebDriverWait(browser, 10)
+
 
 def search():
     try:
@@ -24,15 +29,17 @@ def search():
             EC.presence_of_element_located((By.CSS_SELECTOR, "#q"))
         )
         submit = wait.until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR,"#J_TSearchForm > div.search-button > button"))
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "#J_TSearchForm > div.search-button > button"))
         )
         input.send_keys('美食')
         submit.click()
-        total = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"#mainsrp-pager > div > div > div > div.total")))
+        total = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#mainsrp-pager > div > div > div > div.total")))
         get_products()
         return total.text
     except TimeoutException:
         return search()
+
 
 def next_page(page_number):
     try:
@@ -40,36 +47,52 @@ def next_page(page_number):
             EC.presence_of_element_located((By.CSS_SELECTOR, "#mainsrp-pager > div > div > div > div.form > input"))
         )
         submit = wait.until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "#mainsrp-pager > div > div > div > div.form > span.btn.J_Submit"))
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, "#mainsrp-pager > div > div > div > div.form > span.btn.J_Submit"))
         )
         input.clear()
         input.send_keys(page_number)
         submit.click()
-        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,"#mainsrp-pager > div > div > div > ul > li.item.active > span"),str(page_number)))
+        wait.until(EC.text_to_be_present_in_element(
+            (By.CSS_SELECTOR, "#mainsrp-pager > div > div > div > ul > li.item.active > span"), str(page_number)))
         get_products()
     except TimeoutException:
         next_page(page_number)
 
+
 def get_products():
-    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"#mainsrp-itemlist .items .item")))
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#mainsrp-itemlist .items .item")))
     html = browser.page_source
     doc = pq(html)
     items = doc("#mainsrp-itemlist .items .item").items()
     for item in items:
         product = {
-            'img' : item.find('.pic .img').attr('src'),
+            'img': item.find('.pic .img').attr('src'),
             'price': item.find('.price').text(),
-            'deal': item.find('deal-cnt').text()[:-3],
-            'title':item.find('.title').text(),
-            'shop':item.find('.shop').text(),
-            'location':item.find('.location').text()
+            'deal': item.find('.deal-cnt').text()[:-3],
+            'title': item.find('.title').text(),
+            'shop': item.find('.shop').text(),
+            'location': item.find('.location').text()
         }
-        print(product)
+        # print(product)
+        save_2_mongo(product)
+
+
+def save_2_mongo(result):
+    try:
+        if db[db_collection].insert(result):
+            print('save to db success', result)
+    except Exception:
+        print('fail', result)
+
 
 def main():
     total = search()
     total = int(re.compile('(\d+)').search(total).group(1))
-    for i in range(2,total+1):
+    for i in range(2, total + 1):
         next_page(i)
-if __name__ =='__main__':
+    browser.close()
+
+
+if __name__ == '__main__':
     main()
